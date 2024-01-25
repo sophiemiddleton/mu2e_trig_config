ae4578c23ae44d931070eba5d405af7a0ce042d5
Gianantonio Pezzullo <giani.pezzu@gmail.com>
2024-01-25 11:20:15 -0600

    fixed SCons build

--- python/generateMenuFromJSON.py
+++ python/generateMenuFromJSON.py
@@ -12,13 +12,13 @@ def capitalize(word):
     nn = word[0].upper()
     return nn + word[nLetters:]
 
-def generateLogger(args, dictLog, logName, dictStreams):
+def generateLogger(evtMode, outdir, dictLog, logName, dictStreams):
     name_app = '.fcl'
-    if args.evtMode != 'all':
-        name_app = '_'+args.evtMode+'.fcl'
+    if evtMode != 'all':
+        name_app = '_'+evtMode+'.fcl'
     
-    loggerFileName       = args.outdir+"/"+logName+name_app
-    loggerConfigFileName = args.outdir+"/"+logName+'Config'+name_app
+    loggerFileName       = outdir+"/"+logName+name_app
+    loggerConfigFileName = outdir+"/"+logName+'Config'+name_app
    
     os.system("chmod 755 {}".format(loggerConfigFileName))
     os.system("chmod 755 {}".format(loggerFileName))
@@ -69,21 +69,24 @@ def generateLogger(args, dictLog, logName, dictStreams):
     os.system("chmod 444 {}".format(loggerConfigFileName))
     os.system("chmod 444 {}".format(loggerFileName))
 
+    return [loggerFileName, loggerConfigFileName]
+
 ################################################################################
 ##
 ##
 ##
 ################################################################################
-def generateMenu(args,  dictMenu, menuName, dictStreams, proc_name):
+def generateMenu(evtMode, outdir,  dictMenu, menuName, dictStreams, proc_name):
     name_app = '.fcl'
-    if args.evtMode != 'all':
-        name_app = '_'+args.evtMode+'.fcl'
-    trigMenuFileName = args.outdir+"/"+menuName+name_app
-    psConfigFileName = args.outdir+"/"+menuName+'PSConfig'+name_app
+    if evtMode != 'all':
+        name_app = '_'+evtMode+'.fcl'
+    trigMenuFileName = outdir+"/"+menuName+name_app
+    psConfigFileName = outdir+"/"+menuName+'PSConfig'+name_app
     
     os.system("chmod 755 {}".format(trigMenuFileName))
     os.system("chmod 755 {}".format(psConfigFileName))
 
+    print("trigMenuFileName: {}".format(trigMenuFileName))
     trigMenu = open(trigMenuFileName, 'w')
     psConfig = open(psConfigFileName, 'w')
     
@@ -96,9 +99,9 @@ def generateMenu(args,  dictMenu, menuName, dictStreams, proc_name):
     for k in dictMenu:
         if dictMenu[k]['enabled'] == 0: continue
         evtModes = dictMenu[k]['eventModeConfig']
-        evtModeCheck= args.evtMode == "all"
+        evtModeCheck= evtMode == "all"
         for ll in range(len(evtModes)):
-            if args.evtMode != "all" and args.evtMode == evtModes[ll]["eventMode"]: 
+            if evtMode != "all" and evtMode == evtModes[ll]["eventMode"]: 
                 evtModeCheck = True
                 break
         if evtModeCheck: 
@@ -120,7 +123,7 @@ def generateMenu(args,  dictMenu, menuName, dictStreams, proc_name):
         psInput = "[ "
         notFirst = False
         for ll in range(len(evtModes)):
-            if args.evtMode != "all" and args.evtMode != evtModes[ll]["eventMode"]: continue
+            if evtMode != "all" and evtMode != evtModes[ll]["eventMode"]: continue
             if notFirst: psInput += ","
             psInput += " { eventMode: "+"{}".format(evtModes[ll]["eventMode"])+" prescale:{}".format(evtModes[ll]["prescale"])+"}"
             notFirst = True
@@ -148,11 +151,170 @@ def generateMenu(args,  dictMenu, menuName, dictStreams, proc_name):
     os.system("chmod 444 {}".format(trigMenuFileName))
     os.system("chmod 444 {}".format(psConfigFileName))
 
+    return [psConfigFileName, trigMenuFileName]
+
+#--------------------------------------------------------------------------------
+#    Function used to generate the Menu file in the Offline environemnt
+# 
+#
+#--------------------------------------------------------------------------------
+def generateOffline(menuFile, evtMode, outdir, verbose=False):
 
 
-#def generate(args):
-def generate(raw_args=None):
-    #--------------------------------------------------------------------------------
+    tag = menuFile.split('/')[-1].split('.')[0]
+
+    if verbose :
+        print("configFileText = ",menuFile)
+
+    # when we run from SConscript to create the targets,
+    # we are in the subdir.  When running in Offline scons, we are in Offline,
+    # when in Muse, we are in Offline parent dir
+    # This code makes this always run in the scons default dir, either
+    # Offline or its parent dir
+    srcDir=""
+    outDir=""
+    owd = os.getcwd()
+    if verbose:
+        print ("start owd= ",owd)
+    if 'MUSE_WORK_DIR' in os.environ:
+        # then we are running in Muse, make adjustments
+        if verbose:
+            print ("running in Muse mode ")
+        os.chdir(os.environ['MUSE_WORK_DIR'])
+        srcDir = "mu2e_trig_config/"
+        # like "build/sl7-prof-e20/Offline/"
+        outDir = os.environ['MUSE_BUILD_BASE']+"/mu2e_trig_config/"
+    else:
+        # when we run from SConscript, the owd is the python subdir
+        # but all file name are relative to Offline, so go there
+        words = owd.split("/")
+        if words[-1] == "python" :
+            os.chdir("../..")
+        # accept empty default srcDir and outDir, so build in Offline
+
+    tempArr = menuFile.split("/")
+    tempArr = tempArr[-1].split(".")
+    configFileBaseName = tempArr[0];
+    configFileName     = srcDir+"data/" + configFileBaseName + ".json"
+
+    if verbose:
+        print ("owd = ",owd)
+        print ("pwd = ",os.getcwd())
+        print ("srcDir = ",srcDir)
+        print ("outDir = ",outDir)
+
+    sourceFiles = []
+    targetFiles = []
+
+    sourceFiles.append(configFileName)
+    
+    data_files = [
+        srcDir+'data/physMenu.json',
+        srcDir+'data/extrPosMenu.json'
+    ]
+    for fn in data_files:
+        sourceFiles.append(fn)
+
+    trig_prolog_files = [
+        srcDir+'core/trigDigiInputsEpilog.fcl',
+        srcDir+'core/trigFilters.fcl',
+        srcDir+'core/trigProducers.fcl',
+        srcDir+'core/trigSequences.fcl',
+        srcDir+'core/trigServices.fcl'
+    ]
+    for fn in trig_prolog_files:
+        sourceFiles.append(fn)
+        
+    relProjectDir = "gen"
+    projectDir    = outDir+relProjectDir
+    os.system("mkdir -p {}".format(projectDir))
+
+    with open(menuFile) as f:
+        conf = json.load(f)
+        keys = conf.keys()
+        print("[generateMenuJSON] KEYS FOUND: {}".format(keys))
+        data_streams = {}
+        for k in conf['dataLogger_streams']:
+            data_streams[k] = []
+
+        dict_trkcal_triggers = conf['trigger_paths']
+        trkcal_proc_name     = conf['trkcal_filter_process_name']
+        targetFiles += generateMenu(evtMode, projectDir, dict_trkcal_triggers, 'trig_'+tag, data_streams, trkcal_proc_name)
+        print("[generateMenuJSON] DATA STREAMS FOUND: {}".format(data_streams))        
+
+        dict_agg_triggers = conf['agg_trigger_paths']
+        add_proc_name     = conf['crv_agg_process_name']
+        targetFiles += generateMenu(evtMode, projectDir, dict_agg_triggers, 'agg_'+tag, data_streams, add_proc_name)
+
+        #now produce the logger menus
+        dict_logger = conf['dataLogger_streams']
+        targetFiles += generateLogger(evtMode, projectDir, dict_logger, 'trigLogger_'+tag, data_streams)        
+        #
+        dict_logger = conf['lumiLogger_streams']
+        lumi_streams =  {}
+        lumi_streams['lumi'] = []
+        targetFiles += generateLogger(evtMode, projectDir, dict_logger, 'trigLumiLogger_'+tag, lumi_streams)
+        
+    return sourceFiles, targetFiles, "python "+srcDir+"python/generateMenuFromJSON.py"
+
+    # psConfig.write("}\n")
+    # psConfig.close()
+    # trigMenu.write("  ]\n")
+    # trigMenu.write("}\n")
+    # trigMenu.close()
+    
+    # os.system("chmod 444 {}".format(trigMenuFileName))
+    # os.system("chmod 444 {}".format(psConfigFileName))
+
+    
+
+
+#--------------------------------------------------------------------------------
+#    Function used to generate the Menu file in the Online environment
+#
+#
+#--------------------------------------------------------------------------------
+def generate(args):
+    tag = args.menuFile.split('/')[-1].split('.')[0]
+
+    with open(args.menuFile) as f:
+        conf = json.load(f)
+        keys = conf.keys()
+        print("[generateMenuJSON] KEYS FOUND: {}".format(keys))
+        data_streams = {}
+        for k in conf['dataLogger_streams']:
+            data_streams[k] = []
+
+        dict_trkcal_triggers = conf['trigger_paths']
+        trkcal_proc_name     = conf['trkcal_filter_process_name']
+        generateMenu(args.evtMode, args.outdir, dict_trkcal_triggers, 'trig_'+tag, data_streams, trkcal_proc_name)
+        print("[generateMenuJSON] DATA STREAMS FOUND: {}".format(data_streams))        
+
+        dict_agg_triggers = conf['agg_trigger_paths']
+        add_proc_name     = conf['crv_agg_process_name']
+        generateMenu(args.evtMode, args.outdir, dict_agg_triggers, 'agg_'+tag, data_streams, add_proc_name)
+
+        #now produce the logger menus
+        dict_logger = conf['dataLogger_streams']
+        generateLogger(args.evtMode, args.outdir, dict_logger, 'trigLogger_'+tag, data_streams)        
+        #
+        dict_logger = conf['lumiLogger_streams']
+        lumi_streams =  {}
+        lumi_streams['lumi'] = []
+        generateLogger(args.evtMode, args.outdir, dict_logger, 'trigLumiLogger_'+tag, lumi_streams)
+        
+   
+    # psConfig.write("}\n")
+    # psConfig.close()
+    # trigMenu.write("  ]\n")
+    # trigMenu.write("}\n")
+    # trigMenu.close()
+    
+    # os.system("chmod 444 {}".format(trigMenuFileName))
+    # os.system("chmod 444 {}".format(psConfigFileName))
+
+
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-mf", "--menu-file",
                         dest="menuFile", default="data/physMenu.json",
@@ -167,80 +329,14 @@ def generate(raw_args=None):
                         default="all",
                         help="Specify a single event mode if you want a single-event-mode Menu: OnSpill, OffSpill. The default is 'all'")
 
-    args = parser.parse_args(raw_args)
-
+    args = parser.parse_args()
+    
     os.system("mkdir -p {}".format(args.outdir))
-
+    
     #check the event mode
     allowed_evtModes = ['OnSpill', 'OffSpill', 'all']
     if args.evtMode not in allowed_evtModes:
         print("[generateMenuFromJSON] EVENT-MODE {} NOT ALLOWED! THE POSSIBLE OPTIONS ARE: {}".format(args.evtMode, allowed_evtModes))
         exit(1)
-   
-    #--------------------------------------------------------------------------------
-    tag = args.menuFile.split('/')[-1].split('.')[0]
-
-    with open(args.menuFile) as f:
-        conf = json.load(f)
-        keys = conf.keys()
-        print("[generateMenuJSON] KEYS FOUND: {}".format(keys))
-        data_streams = {}
-        for k in conf['dataLogger_streams']:
-            data_streams[k] = []
-
-        dict_trkcal_triggers = conf['trigger_paths']
-        trkcal_proc_name     = conf['trkcal_filter_process_name']
-        generateMenu(args, dict_trkcal_triggers, 'trig_'+tag, data_streams, trkcal_proc_name)
-        print("[generateMenuJSON] DATA STREAMS FOUND: {}".format(data_streams))        
-
-        dict_agg_triggers = conf['agg_trigger_paths']
-        add_proc_name     = conf['crv_agg_process_name']
-        generateMenu(args, dict_agg_triggers, 'agg_'+tag, data_streams, add_proc_name)
-
-        #now produce the logger menus
-        dict_logger = conf['dataLogger_streams']
-        generateLogger(args, dict_logger, 'trigLogger_'+tag, data_streams)        
-        #
-        dict_logger = conf['lumiLogger_streams']
-        lumi_streams =  {}
-        lumi_streams['lumi'] = []
-        generateLogger(args, dict_logger, 'trigLumiLogger_'+tag, lumi_streams)
         
-   
-    # psConfig.write("}\n")
-    # psConfig.close()
-    # trigMenu.write("  ]\n")
-    # trigMenu.write("}\n")
-    # trigMenu.close()
-    
-    # os.system("chmod 444 {}".format(trigMenuFileName))
-    # os.system("chmod 444 {}".format(psConfigFileName))
-
-
-if __name__ == "__main__":
-    # parser = ArgumentParser()
-    # parser.add_argument("-mf", "--menu-file",
-    #                     dest="menuFile", default="data/physMenu.json",
-    #                     help="Input Menu file")
-    # parser.add_argument("-q", "--quiet",
-    #                     action="store_false", dest="verbose", default=True,
-    #                     help="don't print status messages to stdout")
-    # parser.add_argument("-o", "--outdir",
-    #                     dest="outdir", default="gen",
-    #                     help="Outout directory")
-    # parser.add_argument("-evtMode", "--event-mode",dest="evtMode",
-    #                     default="all",
-    #                     help="Specify a single event mode if you want a single-event-mode Menu: OnSpill, OffSpill. The default is 'all'")
-
-    # args = parser.parse_args()
-    
-    # os.system("mkdir -p {}".format(args.outdir))
-    
-    #check the event mode
-    # allowed_evtModes = ['OnSpill', 'OffSpill', 'all']
-    # if args.evtMode not in allowed_evtModes:
-    #     print("[generateMenuFromJSON] EVENT-MODE {} NOT ALLOWED! THE POSSIBLE OPTIONS ARE: {}".format(args.evtMode, allowed_evtModes))
-    #     exit(1)
-        
-    #generate(args)
-    generate()
+    generate(args)
